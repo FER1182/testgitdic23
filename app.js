963d27b953e4aa2e8e82264fef40b8da461abf75
ferni232 <fernandorudnevich.inedita@gmail.com>
2023-12-11 18:28:54 -0300

    add routes func

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
  const express = require("express")
  const app = express()
+const routes = require("./routes")
 
- const personas= [
-    {
-        nombre:"pepe",
-        eddad:"21"
-    },
-    {
-        nombre:"pepa",
-        eddad:"45"
-    }
- ]
-
-
+app.use("/api",routes);
 
 app.get("/",(req,res)=>{
     res.send("hola mundo!!")
 })
 
-app.get("/personas", (req,res)=>{
-    res.send(personas);
-})
+
 
  app.listen(3000)
